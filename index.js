9ae731944147f36e12139f0c1d0c82f39f324907
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-07-11 18:32:48 -0400

    Connect and disconnect from database/prompts  added on index.js

--- index.js
+++ index.js
@@ -8,3 +8,94 @@ const db = mysql.createConnection({
   password: 'Michelle1990@',
   database: 'employee_management',
 });
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the Employee database.');
+  
+    // Prompt user with main menu questions
+    promptMainMenu();
+  });
+  
+  // Function to prompt the main menu
+  function promptMainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'View employees by manager',
+            'View employees by department',
+            'Delete a department',
+            'Delete a role',
+            'Delete an employee',
+            'View total utilized budget of a department',
+            'Quit',
+          ],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.action) {
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'View employees by manager':
+            viewEmployeesByManager();
+            break;
+          case 'View employees by department':
+            viewEmployeesByDepartment();
+            break;
+          case 'Delete a department':
+            deleteDepartment();
+            break;
+          case 'Delete a role':
+            deleteRole();
+            break;
+          case 'Delete an employee':
+            deleteEmployee();
+            break;
+          case 'View total utilized budget of a department':
+            viewDepartmentBudget();
+            break;
+          case 'Quit':
+            console.log('Good bye!');
+           // End database connection
+            db.end(); 
+            break;
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
